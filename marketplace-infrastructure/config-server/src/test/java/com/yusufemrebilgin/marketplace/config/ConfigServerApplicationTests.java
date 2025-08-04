65b761d5138760905541ba3b2f63bf6d3e8db7fd
Yusuf Emre Bilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2025-08-04 23:55:13 +0300

    feat: setup infra modules with gateway, discovery and config server

---
+++ marketplace-infrastructure/config-server/src/test/java/com/yusufemrebilgin/marketplace/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufemrebilgin.marketplace.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
